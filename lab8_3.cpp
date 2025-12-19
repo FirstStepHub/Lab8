c559ef0cc4b698cd156ed94787cffe0f3bd409b4
FirstStepHub <first_yo1@hotmail.com>
2025-12-19 14:32:32 +0700

    lab8_3_complete.cpp

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,44 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<string>
+using namespace std ;
+
+int age ;
+int height ;
+int bounty ;
+
+int main(){
+
+    cout << "Enter your age: " ;
+    cin >> age ;
+    if(age < 26){
+        cout << "Enter your height: " ;
+        cin >> height ;   
+        if(height < 100){
+            cout << "Your character = Chopper" ;
+            return 0 ;
+        }
+        else if(height >= 100 && height < 180){
+            cout << "Your character = Usopp" ;
+            return 0 ;
+        }
+        else 
+        cout << "Enter your bounty: " ;
+        cin >> bounty ;
+        if(bounty > 1100000000)
+            cout << "Your character = Zoro" ;
+            else  cout << "Your character = Sanji" ;
+            return 0 ;
+
+    }
+    else if(age < 61){
+        cout << "Enter your bounty: " ;
+        cin >> bounty ;
+        if(bounty > 500000000)
+            cout << "Your character = Jinbe" ;
+        else cout << "Your character = Franky" ;
+        return 0 ;
+
+    }
+    else
+    cout << "Your character = Brook" ;
+}
